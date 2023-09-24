383f5ea46d51b6179a3b0468d6ff9b8b19138e5e
samuelzih <samuelz@instaacoders.com>
2023-09-25 02:35:16 +0530

    eighth

--- src/components/FilterPrize.js
+++ src/components/FilterPrize.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 
-function FilterPrize() {
+function FilterPrize({ onSliderChange }) {
 	const [open, setOpen] = useState(false);
+	const [sliderValue, setSliderValue] = useState(50);
+
+	const handleSliderChange = (event) => {
+		const newValue = event.target.value;
+		setSliderValue(newValue);
+		onSliderChange(newValue);
+	};
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+	};
 
 	return (
 		<div className='shadow-sm'>
@@ -52,40 +62,49 @@ function FilterPrize() {
 			</div>
 
 			<div className={`w-full mt-4 ${!open && "hidden"}`}>
-				<div className='w-full'>
-					<div>
-						<input
-							id='small-range'
-							type='range'
-							class='w-full h-1 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm dark:bg-gray-700'
-							min='0'
-							max='50'
-						/>
-					</div>
-
-					<div className='flex space-x-4 font-light items-center w-full justify-center'>
-						<div className='flex space-x-2 items-center'>
-							<button className='py-2 px-3 border'>$299</button>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-								stroke='currentColor'
-								class='w-6 h-6'
-							>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M19.5 12h-15'
-								/>
-							</svg>
-
-							<button className='py-2 px-3 border'>$1266</button>
+				<form>
+					<div className='w-full'>
+						<div>
+							<input
+								id='small-range'
+								type='range'
+								class='w-full h-1 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointer range-sm dark:bg-gray-700'
+								min='35'
+								max='350'
+								value={sliderValue}
+								onChange={handleSliderChange}
+							/>
 						</div>
-						<div className='text-lg font-medium'>go</div>
+
+						<div className='flex space-x-4 font-light items-center w-full justify-center'>
+							<div className='flex space-x-2 items-center'>
+								<div className='py-1 px-3 border'>$35</div>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke-width='1.5'
+									stroke='currentColor'
+									class='w-6 h-6'
+								>
+									<path
+										stroke-linecap='round'
+										stroke-linejoin='round'
+										d='M19.5 12h-15'
+									/>
+								</svg>
+
+								<div className='py-1 px-3 border'>$350</div>
+							</div>
+							<button
+								className='text-lg font-medium capitalize'
+								onClick={(e) => handleFormSubmit(e)}
+							>
+								go
+							</button>
+						</div>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
--- src/components/ResultSideBar.js
+++ src/components/ResultSideBar.js
@@ -6,12 +6,30 @@ import FilterPrize from "./FilterPrize";
 function ResultSideBar(props) {
 	const [selectedOptions, setSelectedOptions] = useState([]);
 	const [selectedOption2, setSelectedOption2] = useState(null);
+	const [sliderValue, setSliderValue] = useState(50);
 
 	const handleSelectedOptionsChange = (newSelectedOptions) => {
 		setSelectedOptions(newSelectedOptions);
 	};
 	const handleSelectedOptionChange2 = (newSelectedOption2) => {
 		setSelectedOption2(newSelectedOption2);
+	}; // Define a callback function to update sliderValue
+	const handleSliderChange = (newValue) => {
+		setSliderValue(newValue);
+	};
+	const handleSelectedOptionClick = (optionValue) => {
+		// When a selected option is clicked, uncheck the corresponding checkbox
+		setSelectedOptions(
+			selectedOptions.filter((value) => value !== optionValue)
+		);
+	};
+	const handleSelectedOptionClick2 = () => {
+		// When a selected option is clicked, unselect it (set to null)
+		setSelectedOption2(null);
+	};
+	const handleRangeClick = () => {
+		// Reset the input range value to the initial value
+		setSliderValue(50);
 	};
 
 	return (
@@ -26,23 +44,82 @@ function ResultSideBar(props) {
 					selectedOption2={selectedOption2}
 					onSelectedOptionChange2={handleSelectedOptionChange2}
 				/>
-				<FilterPrize />
+				<FilterPrize onSliderChange={handleSliderChange} />
 			</div>
 			<div className='flex space-x-2 mt-5 font-light'>
 				<div className='md:hidden space-y-1'>
 					{selectedOptions.map((i, v) => {
-						return <div className='text-xs px-1 bg-slate-200 w-fit'>{i} x</div>;
+						return (
+							<div
+								className='flex justify-between items-center text-xs px-1 bg-slate-200 w-fit'
+								onClick={() => handleSelectedOptionClick(i)}
+							>
+								<div>{i} </div>
+								<div>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 24 24'
+										fill='currentColor'
+										class='w-4 h-4'
+									>
+										<path
+											fill-rule='evenodd'
+											d='M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z'
+											clip-rule='evenodd'
+										/>
+									</svg>
+								</div>
+							</div>
+						);
 					})}
 				</div>
 				<div className='md:hidden space-y-1'>
-					<div className='text-xs px-1 bg-slate-200 w-fit h-fit'>
-						{selectedOption2}
-						{`${selectedOption2 !== null ? " x" : " "}`}
+					<div className='flex justify-between items-center text-xs px-1 bg-slate-200 w-fit h-fit'>
+						<div>{selectedOption2}</div>
+						<div>
+							{selectedOption2 !== null ? (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 24 24'
+									fill='currentColor'
+									class='w-4 h-4 cursor-pointer'
+									onClick={handleSelectedOptionClick2}
+								>
+									<path
+										fill-rule='evenodd'
+										d='M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z'
+										clip-rule='evenodd'
+									/>
+								</svg>
+							) : (
+								" "
+							)}
+						</div>
 					</div>
 				</div>
-				{selectedOption2 !== null && (
-					<div className={` text-xs px-1 bg-slate-200 w-fit h-fit md:hidden `}>
-						$1 - $99
+				{sliderValue && (
+					<div
+						className={`flex justify-between items-center text-xs pl-1 bg-slate-200 w-fit h-fit md:hidden `}
+					>
+						<div>
+							{`${sliderValue !== null ? " $" : " "}`}
+							{sliderValue}
+						</div>
+						<div>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 24 24'
+								fill='currentColor'
+								class='w-4 h-4 cursor-pointer'
+								onClick={handleRangeClick}
+							>
+								<path
+									fill-rule='evenodd'
+									d='M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z'
+									clip-rule='evenodd'
+								/>
+							</svg>
+						</div>
 					</div>
 				)}
 			</div>
