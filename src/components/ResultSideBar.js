91b12f0a22ec2e62e0ded6e6fe928c158c068b7a
samuelzih <samuelz@instaacoders.com>
2023-09-24 21:59:01 +0530

    seventh

--- src/components/ResultSideBar.js
+++ src/components/ResultSideBar.js
@@ -5,7 +5,7 @@ import FilterPrize from "./FilterPrize";
 
 function ResultSideBar(props) {
 	const [selectedOptions, setSelectedOptions] = useState([]);
-	const [selectedOption2, setSelectedOption2] = useState([]);
+	const [selectedOption2, setSelectedOption2] = useState(null);
 
 	const handleSelectedOptionsChange = (newSelectedOptions) => {
 		setSelectedOptions(newSelectedOptions);
@@ -31,21 +31,20 @@ function ResultSideBar(props) {
 			<div className='flex space-x-2 mt-5 font-light'>
 				<div className='md:hidden space-y-1'>
 					{selectedOptions.map((i, v) => {
-						return (
-							<div className='text-xs px-1 bg-slate-200 w-fit'>
-								{i} {} x
-							</div>
-						);
+						return <div className='text-xs px-1 bg-slate-200 w-fit'>{i} x</div>;
 					})}
 				</div>
 				<div className='md:hidden space-y-1'>
 					<div className='text-xs px-1 bg-slate-200 w-fit h-fit'>
 						{selectedOption2}
+						{`${selectedOption2 !== null ? " x" : " "}`}
 					</div>
 				</div>
-				<div className='text-xs px-1 bg-slate-200 w-fit h-fit md:hidden'>
-					$1 - $99
-				</div>
+				{selectedOption2 !== null && (
+					<div className={` text-xs px-1 bg-slate-200 w-fit h-fit md:hidden `}>
+						$1 - $99
+					</div>
+				)}
 			</div>
 		</div>
 	);
